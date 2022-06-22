14b7abd57025ff9e414c504c02576a8ce012db96
Gokhan Turgut <gokhan.turgut@gusto.com>
2022-06-22 17:34:32 +0300

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_141106) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_12_110701) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
