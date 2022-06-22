3417cfbac827b113438afd8aacdd199934fa488f
Gokhan Turgut <gokhan.turgut@gusto.com>
2022-06-22 17:36:07 +0300

    Add user mutation

---
+++ app/graphql/types/auth_provider_credentials_input.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  class AuthProviderCredentialsInput < BaseInputObject
+    # the name is usually inferred by class name but can be overwritten
+    graphql_name 'AUTH_PROVIDER_CREDENTIALS'
+
+    argument :email, String, required: true
+    argument :password, String, required: true
+  end
+end
